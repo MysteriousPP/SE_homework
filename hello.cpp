994d26b0df3b6cde25f7b09438028e49eff6ed65
PP <2553033534@qq.com>
2024-11-16 20:12:32 +0800

    1 -> 114

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello_1"<<endl;
+    cout<<"hello_114"<<endl;
     cout<<"hello_2"<<endl;
     cout<<"hello_3"<<endl;
     cout<<"hello_4"<<endl;
